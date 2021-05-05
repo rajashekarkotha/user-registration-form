73c892676b3d1480b8cb74c16dc1b706a8100329
shekar <kotharajashekar14@gmail.com>
2021-05-05 13:29:50 +0530

    changed file name

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { MustMatch } from '../helpers/must-match.validator';
+import { MustMatch } from '../helpers/password.validator';
 
 @Component({
   selector: 'app-register',
